5c93a8358a532a45cdd0cf1238c6adb17f4c47a1
Juan Rodriguez <juanrodriguezliebana@icloud.com>
2021-10-15 21:07:08 +0200

    fix not object pass type

--- src/api/pass-generator.ts
+++ src/api/pass-generator.ts
@@ -43,7 +43,7 @@ router.post('/api/generatePass', async (req, res, next) => {
 router.post('/api/signPass', async (req, res, next) => {
   try {
     await PassManager.removeStoredPasses()
-    let cert = req.body.cert
+    let cert = JSON.stringify(req.body.cert)
     let manifest = req.body.manifest
 
     let signature = await PassManager.signPass(cert, "12345", manifest)
