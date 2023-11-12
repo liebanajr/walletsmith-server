7d215764fd0cec778f305708b0374779321d29e6
liebanajr <juanrodriguezliebana@gmail.com>
2023-11-12 15:44:35 +0100

    fixed aws ecr login issue

--- push-image.sh
+++ push-image.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]
     echo "No tag supplied"
     exit 1
 fi
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 190396639075.dkr.ecr.eu-west-1.amazonaws.com
+aws ecr get-login-password --region eu-west-1 --profile personal | docker login --username AWS --password-stdin 190396639075.dkr.ecr.eu-west-1.amazonaws.com
 docker buildx create --use
 docker buildx build --platform linux/amd64 --load -t walletsmith-server:$1 .
 docker tag walletsmith-server:$1 190396639075.dkr.ecr.eu-west-1.amazonaws.com/walletsmith-server:$1
